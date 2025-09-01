6f27dc14fabbea7ed3af0d7ca6cff797c386cbbc
bigeye2hu <372931693@qq.com>
2025-09-02 02:33:16 +0800

    🔧 修复MODEL_PATH配置: 更正Docker容器内的模型文件路径

--- analyzer/config.py
+++ analyzer/config.py
@@ -1,7 +1,7 @@
 import os
 
 
-MODEL_PATH = os.getenv("MODEL_PATH", "/data/ClubDetection_1000P_50R.pt")
+MODEL_PATH = os.getenv("MODEL_PATH", "data/ClubDetection_1000P_50R.pt")
 SERVICE_PORT = int(os.getenv("SERVICE_PORT", "5005"))
 
 
